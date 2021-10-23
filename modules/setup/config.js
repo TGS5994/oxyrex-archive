71d3b169d1bb21cd8e562bad02eb871bde36c6e7
woomyAPI <93018166+woomyAPI@users.noreply.github.com>
2021-10-22 21:18:46 -0400

    Update config.js

--- modules/setup/config.js
+++ modules/setup/config.js
@@ -8,7 +8,7 @@ goog.require('goog.structs.PriorityQueue');
 goog.require('goog.structs.QuadTree');
 const defaults = require("../../config.json");
 const gameModeTable = ["FFA", "2TDM", "4TDM", "Domination", /*"Maze Domination",*/ "Mothership", "Tag", /*"Maze", "Maze 2TDM", "Maze 4TDM",*/ "Kill Race", /*"Boss Rush",*/ "Soccer", "Hide and Seek"];
-const gamemode = gameModeTable[(Math.random() * gameModeTable.length | 0)];//gameModeTable[(Math.random() * gameModeTable.length | 0)];
+const gamemode = "Maze 2TDM";//gameModeTable[(Math.random() * gameModeTable.length | 0)];//gameModeTable[(Math.random() * gameModeTable.length | 0)];
 const gamemodes = {
     "Naval Battle": {
         NAVAL_SHIPS: true,
