94dc51244a4d42347fd2a7a72589bf99e955a4d7
z46 <45317380+z46-dev@users.noreply.github.com>
2021-11-18 19:00:11 -0500

    Update config.js

--- modules/setup/config.js
+++ modules/setup/config.js
@@ -63,7 +63,7 @@ function setup(options = {}) {
     return output;
 }
 
-const gamemode = "Mothership";////Math.random() > .75 ? "FFA" : "TDM";
+const gamemode = "Tag";////Math.random() > .75 ? "FFA" : "TDM";
 const gamemodes = {
     "FFA": {}, // "defaults" is already FFA.
     "TDM": (function() {
