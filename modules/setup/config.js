33a879227cb87054c88fff6a32f2ed66da8765cf
z46 <45317380+z46-dev@users.noreply.github.com>
2021-12-07 10:55:55 -0500

    Update config.js

--- modules/setup/config.js
+++ modules/setup/config.js
@@ -456,7 +456,7 @@ const gamemode = (function() {
             throw new ReferenceError(key + " isn't a valid gamemode!");
         }
     }
-    return "Naval Battle";//global.fingerPrint.herokuWA ? "Sandbox" : table[Math.floor(Math.random() * table.length)];
+    return global.fingerPrint.herokuWA ? "Sandbox" : table[Math.floor(Math.random() * table.length)];
 })();
 
 const mode = gamemodes[gamemode];
