2ab93ed12c40fe41b3be1aecb931d211c3ba4ea3
z46 <45317380+z46-dev@users.noreply.github.com>
2021-12-23 09:46:37 -0500

    Update config.js

--- modules/setup/config.js
+++ modules/setup/config.js
@@ -480,7 +480,7 @@ const gamemode = (function() {
             throw new ReferenceError(key + " isn't a valid gamemode!");
         }
     }
-    return global.fingerPrint.herokuWA ? "Boss Rush" : table[Math.floor(Math.random() * table.length)];
+    return global.fingerPrint.herokuWA ? "Sandbox" : table[Math.floor(Math.random() * table.length)];
 })();
 
 const mode = gamemodes[gamemode];
